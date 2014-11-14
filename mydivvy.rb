8ea5d2c5943b283c10e874918272cc12daab39e9
gremiasz <gremiasz@tribune.com>
2014-11-14 01:09:44 -0600

    initial commit

---
+++ mydivvy.rb
@@ -0,0 +1,18 @@
+# mydivvy.rb
+require 'sinatra'
+require 'net/http'
+require 'json'
+
+get '/' do 
+  erb "Hello Divvy User"
+end
+
+get '/getData' do
+  stations = params[:stations] ? JSON.parse(params[:stations]) : []
+  response = Net::HTTP.get(URI 'http://www.divvybikes.com/stations/json')
+  response_json = JSON.parse(response)["stationBeanList"]
+
+  # puts response_json
+  response_json = response_json.reject {|k| !stations.include? k["id"] }
+  erb response_json.to_json, :content_type => 'application/json'
+end
